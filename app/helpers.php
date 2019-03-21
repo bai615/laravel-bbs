d63741cd397cc76b6095e0e1b783f5b3e5e23970
baihua <baihua_2011@163.com>
2019-03-21 22:28:36 +0800

    基础页面结构

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: baihua
- * Date: 2019/3/21
- * Time: 下午9:48
- */
+
+
+// 将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制。
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
