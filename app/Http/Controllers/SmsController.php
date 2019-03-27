860e35122266e7cdb9fae5640075f73e16e856b5
baihua <baihua_2011@163.com>
2019-03-27 21:05:12 +0800

    修正单词拼写

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -17,7 +17,7 @@ class SmsController extends Controller
         $sendSms = new SendSms;
         $sendSms->setPhoneNumbers('phone_number');
         $sendSms->setSignName($config['signName']);
-        $sendSms->setTemplateCode($config['templetCode']);
+        $sendSms->setTemplateCode($config['templateCode']);
         $sendSms->setTemplateParam(['code' => rand(100000, 999999)]);
         $sendSms->setOutId('demo');
 
@@ -29,6 +29,9 @@ class SmsController extends Controller
         官方API文档： https://help.aliyun.com/document_detail/55451.html?spm=5176.doc55289.6.556.pMlBIe
         composer： https://getcomposer.org/
 
+         * stdClass Object ( [Message] => OK [RequestId] => D7E470CD-A3F0-48BB-ADCC-61E309A22756 [BizId] => 326404353691140716^0 [Code] => OK )
+         *
+         * stdClass Object ( [Message] => phone_number invalid mobile number [RequestId] => 907CDC3C-5318-4593-AE7F-0832BA334C0B [Code] => isv.MOBILE_NUMBER_ILLEGAL )
          */
 
         return response('OK');
--- config/sms.php
+++ config/sms.php
@@ -3,5 +3,5 @@ return [
     'accessKeyId'    => env('ALIYUN_ACCESS_KEY_ID'),
     'accessKeySecret' => env('ALIYUN_ACCESS_KEY_SECRET'),
     'signName' => env('ALIYUN_SIGN_NAME'),
-    'templetCode' => env('ALIYUN_TEMPLET_CODE'),
+    'templateCode' => env('ALIYUN_TEMPLATE_CODE'),
 ];
