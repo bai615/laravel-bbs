65668a2b112827e85fe24afd84be06c6a28b1b22
baihua <baihua_2011@163.com>
2019-03-28 16:34:57 +0800

    add captcha

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.28 on 2019-03-27 21:13:42.
+ * Generated for Laravel 5.7.28 on 2019-03-28 15:57:49.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- routes/api.php
+++ routes/api.php
@@ -30,12 +30,15 @@ $api->version('v1', [
         'middleware' => 'api.throttle', // 调用频率限制，1分钟1次
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
-    ], function ($api){
+    ], function ($api) {
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
         // 用户注册
         $api->post('users', 'UsersController@store')
             ->name('api.users.store');
+        // 图片验证码
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
     });
 });
