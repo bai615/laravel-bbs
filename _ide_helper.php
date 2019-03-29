d201bb9d9f7debe749069964ec2a71db1b58da50
baihua <baihua_2011@163.com>
2019-03-29 21:44:38 +0800

    social authorizations

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.28 on 2019-03-28 20:52:34.
+ * Generated for Laravel 5.7.28 on 2019-03-29 21:29:19.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
