523e3b0aee4b723c7e744ffbf5533dd7284bb212
baihua <baihua_2011@163.com>
2019-04-02 21:59:07 +0800

    social login change to Passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Models\User;
+
 use Illuminate\Http\Request;
 use Auth;
 use League\OAuth2\Server\AuthorizationServer;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response as Psr7Response;
+use App\Traits\PassportToken;
 
 class AuthorizationsController extends Controller
 {
+    use PassportToken;
+
     public function socialStore($type, SocialAuthorizationRequest $request)
     {
         if (!in_array($type, ['weixin'])){
@@ -62,9 +66,12 @@ class AuthorizationsController extends Controller
                 break;
         }
 
-        $token = Auth::guard('api')->fromUser($user);
+//        $token = Auth::guard('api')->fromUser($user);
 
-        return $this->respondWithToken($token)->setStatusCode(201);
+//        return $this->respondWithToken($token)->setStatusCode(201);
+
+        $result = $this->getBearerTokenByUser($user, '1', false);
+        return $this->response->array($result)->setStatusCode(201);
     }
 
     public function store(AuthorizationRequest $originRequest, AuthorizationServer $server, ServerRequestInterface $serverRequest)
@@ -76,31 +83,31 @@ class AuthorizationsController extends Controller
         }
     }
 
-    public function store_bak(AuthorizationRequest $request)
-    {
-        $username = $request->username;
+//    public function store_bak(AuthorizationRequest $request)
+//    {
+//        $username = $request->username;
+//
+//        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+//            $credentials['email'] = $username :
+//            $credentials['phone'] = $username;
+//
+//        $credentials['password'] = $request->password;
+//
+//        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+//            return $this->response->errorUnauthorized(trans('auth.failed'));
+//        }
+//
+//        return $this->respondWithToken($token)->setStatusCode(201);
+//    }
 
-        filter_var($username, FILTER_VALIDATE_EMAIL) ?
-            $credentials['email'] = $username :
-            $credentials['phone'] = $username;
-
-        $credentials['password'] = $request->password;
-
-        if (!$token = \Auth::guard('api')->attempt($credentials)) {
-            return $this->response->errorUnauthorized(trans('auth.failed'));
-        }
-
-        return $this->respondWithToken($token)->setStatusCode(201);
-    }
-
-    protected function respondWithToken($token)
-    {
-        return $this->response->array([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
-        ]);
-    }
+//    protected function respondWithToken($token)
+//    {
+//        return $this->response->array([
+//            'access_token' => $token,
+//            'token_type' => 'Bearer',
+//            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+//        ]);
+//    }
 
     public function update(AuthorizationServer $server, ServerRequestInterface $serverRequest)
     {
@@ -111,11 +118,11 @@ class AuthorizationsController extends Controller
         }
     }
 
-    public function update_bak()
-    {
-        $token = Auth::guard('api')->refresh();
-        return $this->respondWithToken($token);
-    }
+//    public function update_bak()
+//    {
+//        $token = Auth::guard('api')->refresh();
+//        return $this->respondWithToken($token);
+//    }
 
     public function destroy()
     {
@@ -127,9 +134,9 @@ class AuthorizationsController extends Controller
         }
     }
 
-    public function destroy_bak()
-    {
-        Auth::guard('api')->logout();
-        return $this->response->noContent();
-    }
+//    public function destroy_bak()
+//    {
+//        Auth::guard('api')->logout();
+//        return $this->response->noContent();
+//    }
 }
